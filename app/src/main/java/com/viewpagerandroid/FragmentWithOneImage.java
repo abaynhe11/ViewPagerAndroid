8a2f296db35b25f86bb53783318dbdc9ba86c638
Abbas Suterwala <abbas.s786@yahoo.co.in>
2016-06-05 17:06:45 +0530

    Added view pager functionality

---
+++ app/src/main/java/com/viewpagerandroid/FragmentWithOneImage.java
@@ -0,0 +1,44 @@
+package com.viewpagerandroid;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class FragmentWithOneImage extends Fragment {
+    private String title;
+    private int image;
+
+
+    public static FragmentWithOneImage newInstance(String title, int resImage) {
+        FragmentWithOneImage fragment = new FragmentWithOneImage();
+        Bundle args = new Bundle();
+        args.putInt("image", resImage);
+        args.putString("title", title);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        image = getArguments().getInt("image", 0);
+        title = getArguments().getString("title");
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_one_img, container, false);
+        TextView tvLabel = (TextView) view.findViewById(R.id.txtMain);
+        tvLabel.setText(title);
+
+        ImageView imageView = (ImageView) view.findViewById(R.id.imgMain);
+        imageView.setImageResource(image);
+        return view;
+    }
+}
--- app/src/main/java/com/viewpagerandroid/MainActivity.java
+++ app/src/main/java/com/viewpagerandroid/MainActivity.java
@@ -1,17 +1,28 @@
 package com.viewpagerandroid;
 
-import android.support.v7.app.ActionBarActivity;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import com.ToxicBakery.viewpager.transforms.RotateUpTransformer;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
+    FragmentPagerAdapter adapterViewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ViewPager vpPager = (ViewPager) findViewById(R.id.vpPager);
+        adapterViewPager = new MyPagerAdapter(getSupportFragmentManager());
+        vpPager.setAdapter(adapterViewPager);
+        vpPager.setPageTransformer(true, new RotateUpTransformer());
     }
 
     @Override
@@ -35,4 +46,43 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+    public static class MyPagerAdapter extends FragmentPagerAdapter {
+        private static int NUM_ITEMS = 3;
+
+        public MyPagerAdapter(FragmentManager fragmentManager) {
+            super(fragmentManager);
+        }
+
+        // Returns total number of pages
+        @Override
+        public int getCount() {
+            return NUM_ITEMS;
+        }
+
+        // Returns the fragment to display for that page
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return FragmentWithOneImage.newInstance("Fragment 1", R.drawable.android_1);
+                case 1:
+                    return FragmentWithOneImage.newInstance("Fragment 2", R.drawable.android_2);
+                case 2:
+                    return FragmentWithTwoImages.newInstance("Fragment 3", R.drawable.android_3, R.drawable.android_4);
+                default:
+                    return null;
+            }
+        }
+
+        // Returns the page title for the top indicator
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return "Tab " + position;
+        }
+
+    }
+
 }
+
